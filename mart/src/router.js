c8f0ef6a4b1590626e7d448da4dffe28b1809d60
MiBoCun <Midadada@aliyun.com>
2020-10-24 22:47:01 +0800

    vue-router

--- mart/src/router.js
+++ mart/src/router.js
@@ -1,4 +1,4 @@
-import Vue from "vue"
+import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './view/Home'
 import Cart from './view/Cart'
@@ -6,16 +6,18 @@ import Cart from './view/Cart'
 Vue.use(VueRouter)
 
 let router = new VueRouter({
-    routes: [{
-        path:'/',
-        component:Home,
-        name:'home'
-    },
-    {
-        path:'/cart',
-        component:Cart,
-        name:'cart'
-    },
+    routers: [{
+            path: '/',
+            components: Home,
+            name: 'name'
+        },
+        {
+            path: '/cart',
+            components: Cart,
+            name: 'cart'
+
+        }
+
     ]
 })
 export default router
