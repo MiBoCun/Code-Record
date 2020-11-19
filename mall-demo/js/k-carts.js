3f344301f8ec2b28e7bfb1342939a3109409627a
MiBoCun <Midadada@aliyun.com>
2020-11-19 22:06:05 +0800

    control the number of items in the shopping cart

--- mall-demo/js/k-carts.js
+++ mall-demo/js/k-carts.js
@@ -7,18 +7,20 @@ Vue.component('k-carts',{
             },
             set (newvalue) {
                 this.$emit('ischeckall',newvalue)
-
             }  
+        },
+        isPay:{
+            get ( ){
+                return this.cartsitmes.find(item => item.checked === true)
+            }
         }
     },
     template:`
-    <div id="cartForm">
-   
+    <div id="cartForm">   
         <div id="formTitle">
         <ul>
-            <li> 
-                
-                <input type="checkbox" v-model="checkAll">
+            <li>                 
+                <input type="checkbox" v-model="checkAll">               
                 <label for="">全选</label>
             </li>
             <li style="width:34px;">商品</li>
@@ -38,14 +40,13 @@ Vue.component('k-carts',{
                    <div class="imgLi">
                      <img :src=cart.cover :title=cart.title>
                    </div>
-                   <div style="width:200px;">{{cart.title}}</div>
-                   
+                   <div style="width:200px;">{{cart.title}}</div>                   
                    <div style="width:95px;">{{cart.price}}</div>
-                   <div class="divCount" >
-                     <button >-</button>
-                     <input type="text" :value=cart.count>
-                     <button >+</button></div>
-                   <div style="width:150px;">{{cart.price * cart.count}}</div>
+                   <div class="divCount" >                  
+                     <button @click="changeCount(cart.count-1,cart)">-</button>
+                     <input type="text" :value=cart.count @blur="changeCount($event.target.value,cart)">
+                     <button @click="changeCount(cart.count+1,cart)">+</button></div>
+                   <div style="width:150px;">{{(cart.price * cart.count).toFixed(2)}}</div>
                    <div class="divOperate" > <button>删除</button></div>
                 </li>               
             </ul>
@@ -53,14 +54,21 @@ Vue.component('k-carts',{
         <div id='formTotal'>
          <span>一共选择了  件商品，</span>
          <span>总计    元</span>
-         <div id="payButton">立即支付</div>
+         <button class="payButton" :class="{payActive: isPay}" :disabled="!isPay" @click="payclick">立即支付</button>
         </div>
     </div>`,
     methods: {
         chagneChecked (cart,e) {
             this.$emit("changecheck",cart,e.target.checked)
-            console.log(e.target.checked)
+           
+        },
+        changeCount (count,cart) {
+            console.log(count,cart)
+            this.$emit('changecount',count,cart)
 
+        },
+        payclick ( ) {
+            alert("我可以点击了")
         }
 
     }
